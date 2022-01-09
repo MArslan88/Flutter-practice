2317ae7aaef32368fafd30ad7384563b4d46a5fb
MArslan88 <gery_maz_555@hotmail.com>
2022-01-09 15:47:03 +0500

    updated

---
+++ operator.dart
@@ -0,0 +1,13 @@
+void main() {
+  int num1 = 50;
+
+  if (num1 >= 80) {
+    print("A1");
+  } else if (num1 >= 70) {
+    print("A");
+  } else if (num1 >= 60) {
+    print("B");
+  } else {
+    print("Fail");
+  }
+}
