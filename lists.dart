4a21452bb70c0951dc5b13658f85bf891c1c8c8b
MArslan88 <gery_maz_555@hotmail.com>
2022-01-09 15:15:50 +0500

    update

---
+++ lists.dart
@@ -0,0 +1,23 @@
+void main() {
+  // List<String> name = ["Arslan", "Danish"];
+
+  // List dynamic = ["Tanzeel", 2, 5.6, false, null];
+
+  // print(dynamic[0]);
+
+  List<String> name = ["Danish", "Arslan", "Tanzeel", "Noman"];
+  // print(name);
+
+  // name[0] = "Kashif";
+
+  // print(name);
+
+  // Two dimentional List
+
+  List<List<String>> sections = [
+    name,
+    ["123", "345"]
+  ];
+
+  print(sections[1][0]);
+}
