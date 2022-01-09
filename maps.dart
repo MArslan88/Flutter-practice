7776fc231ad9ef2ac9697c29965737a84b7d71b4
MArslan88 <gery_maz_555@hotmail.com>
2022-01-09 15:32:47 +0500

    updated

---
+++ maps.dart
@@ -0,0 +1,9 @@
+void main() {
+  Map<String, int> marks = {"Arslan": 50, "Danish": 40};
+  // print(marks["Danish"]);
+
+  List<Map<String, int>> marks2 = [
+    marks,
+    {"Noman": 50, "Kashif": 60}
+  ];
+}
