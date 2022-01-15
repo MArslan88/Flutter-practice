5236dfb069aee6a1b241fffb893f6bcd28603acc
MArslan88 <gery_maz_555@hotmail.com>
2022-01-15 10:43:17 +0500

    Assignment-2

---
+++ list_assignment.dart
@@ -0,0 +1,13 @@
+void main() {
+  List<Map<String, String>> profile = [
+    {"profileId": "1", "userName": "M Arslan", "userEmail": "arslan@gmail.com"},
+    {
+      "profileId": "2",
+      "userName": "Danish Nisar",
+      "userEmail": "danish@gmail.com"
+    },
+    {"profileId": "3", "userName": "Tanzeel", "userEmail": "tanzeel@gmail.com"}
+  ];
+
+  print(profile[0]);
+}
