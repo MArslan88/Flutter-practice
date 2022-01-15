f3439d2aca4ee86797e10955c7cdec7afcdf1410
MArslan88 <gery_maz_555@hotmail.com>
2022-01-15 15:40:15 +0500

    for loop

---
+++ for_loop.dart
@@ -0,0 +1,8 @@
+void main() {
+  List<String> name = ["Arslan", "Danish", "Tanzeel"];
+  // print(name);
+
+  for (int a = 0; a < name.length; a++) {
+    print("Student ${a + 1}: ${name[a]}");
+  }
+}
